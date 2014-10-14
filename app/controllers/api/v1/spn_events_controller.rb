b300e526bf78261d162332aacde1502eda559d3c
“Kyle <kschmit90@gmail.com>
2014-10-14 15:06:22 -0500

    WIP

--- app/controllers/api/v1/spn_events_controller.rb
+++ app/controllers/api/v1/spn_events_controller.rb
@@ -1,22 +1,3 @@
 class SpnEventsController < ActionController::API
   
-  # Event Page
-  # page = Nokogiri::HTML(open('http://siliconprairienews.com/events/list/?action=tribe_list&tribe_paged=1&tribe_event_display=past'))
-    
-  # Event Name
-  # page.css('.url').each do |item|
-  # end
-  
-  # Event Date/Time Details
-  # page.css('.time-details').each do |item|
-  # end
-  
-  # Event Location Details
-  # page.css('.tribe-events-venue-details').each do |item|
-  # end
-  
-  # Mechanize 
-  # agent = Mechanize.new
-  # page = agent.get('http://siliconprairienews.com/events/list/?action=tribe_list&tribe_paged=1&tribe_event_display=list')
-  # next_page = agent.page.link_with(:text => "Next Events »").click
 end
--- app/models/spn_event.rb
+++ app/models/spn_event.rb
@@ -1,6 +1,35 @@
 class SpnEvent < ActiveRecord::Base
   attr_accessible :date, :location, :name, :time, :url
   
-  # Takes a url as a parameter and returns a Nokogiri object with the page contents
+  def self.run_spn_scraper
+    page_num = 1
+    while OpenURI::HTTPError != '404 Not Found' do
+      page = Nokogiri::HTML
+      page.css('.vevent').each do |event|
+        Event.create(name: event.event_name)
+  end
+  
+  # Name of Event
+  page.css('.vevent')[0].css('.url').text.strip
+  # Url
+  page.css('.vevent')[0].css('.url')[0]['href']
+  # Date Start
+  page.css('.vevent')[0].css('.dtstart')[0].children[0].text
+  # End Time
+  page.css('.vevent')[0].css('.dtend')[0].children.text
+  # Event Author
+  page.css('.vevent')[0].css('.tribe-events-venue-details')[0].children.css('.author').text
+  # Event Address
+  page.css('.vevent')[0].css('.tribe-events-venue-details')[0].children.css('.street-address').text
+  # Location
+  page.css('.vevent')[0].css('.tribe-events-venue-details')[0].children.css('.locality').text
+  # Region
+  page.css('.vevent')[0].css('.tribe-events-venue-details')[0].children.css('.region').text
+  # Postal Code
+  page.css('.vevent')[0].css('.tribe-events-venue-details')[0].children.css('.postal-code').text
+  # Country
+  page.css('.vevent')[0].css('.tribe-events-venue-details')[0].children.css('.country-name').text
+  # Description
+  page.css('.vevent')[0].css('.description')[0].css('p').text
   
 end
