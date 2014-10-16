7cb25c29e670458633cdb1eed599f5f352835fd4
“Kyle <kschmit90@gmail.com>
2014-10-16 10:20:00 -0500

    incrementing month calendars

--- lib/tech_omaha_scraper.rb
+++ lib/tech_omaha_scraper.rb
@@ -11,28 +11,27 @@ module TechOmahaScraper
   # Refactor
   
   def self.scrape
-    month = Time.now.month
+    this_month = Time.now.month
     agent = Mechanize.new
-    url = "https://www.google.com/calendar/htmlembed?height=600&wkst=1&bgcolor=%23ff6666&src=689bo9l4k74mu9unjbqtnulpn0@group.calendar.google.com&color=%23A32929&ctz=America/Chicago&dates=2014#{month}01/2014#{TechOmahaScraper.next_month}01&mode=MONTH"
-    page = agent.get(url)
+    url1  = "https://www.google.com/calendar/htmlembed?height=600&wkst=1&bgcolor=%23ff6666&src=689bo9l4k74mu9unjbqtnulpn0@group.calendar.google.com&color=%23A32929&ctz=America/Chicago&dates=2014#{this_month}01/2014#{TechOmahaScraper.next_month(this_month)}01&mode=MONTH"
+    url2  = "https://www.google.com/calendar/htmlembed?height=600&wkst=1&bgcolor=%23ff6666&src=689bo9l4k74mu9unjbqtnulpn0@group.calendar.google.com&color=%23A32929&ctz=America/Chicago&dates=2014#{this_month + 1}01/2014#{TechOmahaScraper.next_month(this_month + 1)}01&mode=MONTH"
+    page  = agent.get(url)
     links = TechOmahaScraper.prep_links(page)
-    
     
     links.each do |link|
       clicked_link = link.click.parser
       Event.create(
-        event_name: TechOmahaScraper.event_name(clicked_link),
+        event_name:        TechOmahaScraper.event_name(clicked_link),
         event_description: TechOmahaScraper.event_description(clicked_link),
-        event_date: TechOmahaScraper.event_date(clicked_link),
-        event_end: TechOmahaScraper.event_end(clicked_link),
-        event_address: TechOmahaScraper.event_location(clicked_link),
-        event_origin: 'Tech_Omaha')
+        event_date:        TechOmahaScraper.event_date(clicked_link),
+        event_end:         TechOmahaScraper.event_end(clicked_link),
+        event_address:     TechOmahaScraper.event_location(clicked_link),
+        event_origin:      'Tech_Omaha')
     end
   end
   
-  def self.next_month
-    this_month = Time.now.month
-    if this_month == 12
+  def self.next_month(month)
+    if month == 12
       next_month = '01'
     else
       next_month = this_month + 1
