fc3289ed5d555d61147e03b64fe3ea4955856a63
“Kyle <kschmit90@gmail.com>
2014-10-16 13:26:20 -0500

    removed pry

--- lib/spn_scraper.rb
+++ lib/spn_scraper.rb
@@ -22,7 +22,6 @@ module SpnScraper
       page = Nokogiri::HTML(open(url))
       
       page.css('.vevent').each do |event|
-        binding.pry
         Event.create(
           event_name:        SpnScraper.event_name(event), 
           event_date:        SpnScraper.event_date(event),
