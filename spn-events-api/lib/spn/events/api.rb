1e73547a2375dfedfd7e8ef80d14065047671134
“Kyle <kschmit90@gmail.com>
2014-10-16 18:47:21 -0500

    created gem base

---
+++ spn-events-api/lib/spn/events/api.rb
@@ -0,0 +1,9 @@
+require "spn/events/api/version"
+
+module Spn
+  module Events
+    module Api
+      # Your code goes here...
+    end
+  end
+end
---
+++ spn-events-api/lib/spn/events/api/version.rb
@@ -0,0 +1,7 @@
+module Spn
+  module Events
+    module Api
+      VERSION = "0.0.1"
+    end
+  end
+end
