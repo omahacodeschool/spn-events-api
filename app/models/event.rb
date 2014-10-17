5f9454252253b0d3b6207e19330e9930663d23c6
“Kyle <kschmit90@gmail.com>
2014-10-17 15:08:33 -0500

    events sorting by date

--- app/models/event.rb
+++ app/models/event.rb
@@ -16,19 +16,18 @@ class Event < ActiveRecord::Base
     spn_events = []
     spn_array = Event.where(event_origin: 'Silicon_Prairie_News').order(:event_date)
     spn_array.each do |event|
-      binding.pry
       if Time.now.yesterday <= event.event_date.to_s.slice(0..9)
         spn_events << event
       else
         next
       end
     end
-    spn_events
+    spn_events.sort { |a, b| a.event_date. <=> b.event_date }
   end
   
   def self.lincoln_events
     lincoln_events = []
-    lincoln_array = Event.where(event_origin: 'Startup_Lincoln')
+    lincoln_array = Event.where(event_origin: 'Startup_Lincoln').order(:event_date)
     lincoln_array.each do |event|
       if Time.now.yesterday <= event.event_date.to_s.slice(0..9)
         lincoln_events << event
@@ -36,12 +35,12 @@ class Event < ActiveRecord::Base
         next
       end
     end
-    lincoln_events
+    lincoln_events.sort { |a, b| a.event_date. <=> b.event_date }
   end
   
   def self.tech_omaha_events
     tech_omaha_events = []
-    tech_omaha_array = Event.where(event_origin: 'Tech_Omaha')
+    tech_omaha_array = Event.where(event_origin: 'Tech_Omaha').order(:event_date)
     tech_omaha_array.each do |event|
       if Time.now.yesterday <= event.event_date.to_s.slice(0..9)
         tech_omaha_events << event
@@ -49,31 +48,31 @@ class Event < ActiveRecord::Base
         next
       end
     end
-    tech_omaha_events
+    tech_omaha_events.sort { |a, b| a.event_date. <=> b.event_date }
   end
   
   def self.all_events
     all_events = []
     Event.all.each do |event|
-      if Time.now <= event.event_date.to_s.slice(0..9)
+      if Time.now.yesterday <= event.event_date.to_s.slice(0..9)
         all_events << event
       else
         next
       end
     end
-    all_events
+    all_events.sort { |a, b| a.event_date. <=> b.event_date }
   end
   
   def self.same_week
     rest_of_weeks_events = []
     Event.all.each do |event|
-      if Time.now <= event.event_date.to_s.slice(0..9) && event.event_date.to_s.slice(0..9) < Time.now.end_of_week
+      if Time.now.yesterday <= event.event_date.to_s.slice(0..9) && event.event_date.to_s.slice(0..9) < Time.now.end_of_week
         rest_of_weeks_events << event
       else  
         next
       end  
     end
-    rest_of_weeks_events
+    rest_of_weeks_events.sort { |a, b| a.event_date. <=> b.event_date }
   end
   
   def self.week_of_events
@@ -85,19 +84,19 @@ class Event < ActiveRecord::Base
         next
       end
     end
-    weeks_events
+    weeks_events.sort { |a, b| a.event_date. <=> b.event_date }
   end
   
   def self.events_today
     todays_events = []
     Event.all.each do |event|
-      if event.event_date.to_s.slice(0..9) == Time.now
+      if event.event_date.to_s.slice(0..9) == Time.now.yesterday
         todays_events << event
       else
         next
       end
     end
-    todays_event
+    todays_event.sort { |a, b| a.event_date. <=> b.event_date }
   end
   
   def self.events_this_month
@@ -109,7 +108,7 @@ class Event < ActiveRecord::Base
         next
       end
     end
-    months_events
+    months_events.sort { |a, b| a.event_date <=> b.event_date }
   end
   
   def self.events_by_month(month)
@@ -121,7 +120,7 @@ class Event < ActiveRecord::Base
         next
       end
     end
-    events_for_month
+    events_for_month.sort { |a, b| a.event_date. <=> b.event_date }
   end
   
   def self.past_events
@@ -133,6 +132,6 @@ class Event < ActiveRecord::Base
         next
       end
     end
-    past_events
+    past_events.sort { |a, b| a.event_date. <=> b.event_date }
   end
 end
